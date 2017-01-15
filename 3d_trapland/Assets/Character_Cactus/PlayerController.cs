e65f4aa5a646d67ebf0af9f35c8a1a7925c9400c
Mark <bozsokimark1@gmail.com>
2017-01-15 15:24:39 +0100

    Basic movement

---
+++ 3d_trapland/Assets/Character_Cactus/PlayerController.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+    public float speed = 10.0f;
+    private float playerX;
+    private float playerZ;
+
+    // Use this for initialization
+    void Start () {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        playerX = Input.GetAxis("Vertical") * speed;
+        playerZ = Input.GetAxis("Horizontal") * speed;
+        playerX *= Time.deltaTime;
+        playerZ *= Time.deltaTime;
+
+        transform.Translate(playerZ, 0, playerX);
+
+        if (Input.GetKeyDown("escape"))
+            Cursor.lockState = CursorLockMode.None;
+    }
+}
