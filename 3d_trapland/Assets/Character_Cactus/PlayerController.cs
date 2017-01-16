da86896c39c187cab68e5e0f6070dc1a2952bf9d
Mark <bozsokimark1@gmail.com>
2017-01-16 15:05:07 +0100

    Jump and EnemyController

--- 3d_trapland/Assets/Character_Cactus/PlayerController.cs
+++ 3d_trapland/Assets/Character_Cactus/PlayerController.cs
@@ -1,18 +1,44 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
-public class PlayerController : MonoBehaviour {
+public class PlayerController : MonoBehaviour
+{
     public float speed = 10.0f;
+    public float jumpForce = 8.0f;
     private float playerX;
     private float playerZ;
+    private float distToGround;
+
+    private bool isAttack;
+
+    private Rigidbody rb;
+
+    public Text HPText;
+    private float HealthPoints = 100;
+
+    private GameObject enemy;
+    private Vector3 AttackRadius = new Vector3(5.0f, 0.0f, 5.0f);
 
     // Use this for initialization
-    void Start () {
+    void Start()
+    {
         Cursor.lockState = CursorLockMode.Locked;
+        rb = GetComponent<Rigidbody>();
+
+        HPText.text = HealthPoints.ToString();
+
+        enemy = GameObject.FindGameObjectWithTag("Enemy");
     }
-	
-	// Update is called once per frame
-	void Update () {
+
+    bool IsGrounded()
+    {
+        return Physics.Raycast(transform.position, -Vector3.up, distToGround + 0.1f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
         playerX = Input.GetAxis("Vertical") * speed;
         playerZ = Input.GetAxis("Horizontal") * speed;
         playerX *= Time.deltaTime;
@@ -22,5 +48,33 @@ public class PlayerController : MonoBehaviour {
 
         if (Input.GetKeyDown("escape"))
             Cursor.lockState = CursorLockMode.None;
+
+        if (Input.GetKeyDown(KeyCode.Space) && IsGrounded())
+        {
+            rb.AddForce(new Vector3(0, jumpForce, 0), ForceMode.Impulse);
+        }
+
+        /*if (Input.GetMouseButtonDown(1))
+        {
+            
+        }*/
+        if (Input.GetMouseButtonDown(0))
+        {
+            if ((Mathf.Abs(enemy.transform.position.x - transform.position.x)) < AttackRadius.x && (Mathf.Abs(enemy.transform.position.y - transform.position.y)) < AttackRadius.y)
+            {
+                enemy.GetComponent<EnemyController>().Damaged();
+                Debug.Log("Enemy damaged");
+            }
+        }
+    }
+
+    public void Damaged()
+    {
+        HealthPoints -= 1;
+        HPText.text = Mathf.Round(HealthPoints).ToString();
+        if (HealthPoints <= 0)
+        {
+            GameObject.Destroy(gameObject);
+        }
     }
 }
---
+++ 3d_trapland/Assets/EnemyController.cs
@@ -0,0 +1,71 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class EnemyController : MonoBehaviour
+{
+    private NavMeshAgent NavMeshAgent;
+    private Animator Animator;
+    private float StartKillTime;
+    private int NextWaypoint = 0;
+
+    public List<Transform> Checkpoints;
+
+    private GameObject player;
+
+    // Use this for initialization
+    void Start()
+    {
+        NavMeshAgent = GetComponent<NavMeshAgent>();
+        Animator = GetComponent<Animator>();
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (NavMeshAgent.enabled == true)
+        {
+            NavMeshAgent.SetDestination(Checkpoints[NextWaypoint].position);
+
+            if (Vector3.Distance(transform.position, NavMeshAgent.destination) <= NavMeshAgent.stoppingDistance)
+            {
+                ++NextWaypoint;
+                if (NextWaypoint >= Checkpoints.Count)
+                {
+                    NextWaypoint++;
+                }
+            }
+
+            var player = GameObject.FindGameObjectWithTag("Player");
+            if (Vector3.Distance(transform.position, player.transform.position) <= 4.0f)
+            {
+                NavMeshAgent.SetDestination(player.transform.position);
+            }
+
+            if (Vector3.Distance(transform.position, player.transform.position) <= NavMeshAgent.stoppingDistance)
+            {
+                player.GetComponent<PlayerController>().Damaged();
+            }
+        }
+        else
+        {
+            if (Time.time - StartKillTime > 4.0f)
+            {
+                GameObject.Destroy(transform.gameObject);
+            }
+        }
+    }
+
+    public void Damaged()
+    {
+        NavMeshAgent.enabled = false;
+        Animator.SetTrigger("Kill");
+        StartKillTime = Time.time;
+    }
+
+    public void StartSinking()
+    {
+
+    }
+}
